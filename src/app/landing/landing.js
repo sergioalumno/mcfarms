abc40abc7b6763aac597880cce0e1cfa9dfb0887
Sergio Lopez Gil <slopgil0612@g.educaand.es>
2025-12-08 20:03:28 +0100

    comit final final

--- src/app/landing/landing.js
+++ src/app/landing/landing.js
@@ -3,9 +3,7 @@ export default function Landing() {
     return (
         <div>
             <h1>MC Farms</h1>
-            <img src="logo.png"/>
-            <img src="flecha.png"/>
-            <img src="granjalanding.webp"/>
+            <img src="fotolanding.png"/>
             <p>Todo esto y mas aquí ¡Empieza ahora!</p>
             <button>Iniciar Sesión</button>
             <button>Registrase</button>
--- src/app/tuperfil/tuperfil.js
+++ src/app/tuperfil/tuperfil.js
@@ -41,8 +41,4 @@ const [editar, setEditar] = useState(false)
             <button>Proyectos guardados</button>
         </div>
     )
-
-
-
 }
-
